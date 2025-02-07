1c3af8e18d9a5fd1eedfaa7471cbb4208b1e10d7
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-02-07 09:17:22 -0600

    update footer and topbar components; remove unused code and update branding

--- mqml-frontend-workspace/src/app/layout/component/menu/menu.component.ts
+++ mqml-frontend-workspace/src/app/layout/component/menu/menu.component.ts
@@ -87,50 +87,50 @@ export class MenuComponent implements OnInit {
           }
         ]
       },
-      {
-        label: 'Hierarchy',
-        items: [
-          {
-            label: 'Submenu 1',
-            icon: 'pi pi-fw pi-bookmark',
-            items: [
-              {
-                label: 'Submenu 1.1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                  { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                  { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                  { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                ]
-              },
-              {
-                label: 'Submenu 1.2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-              }
-            ]
-          },
-          {
-            label: 'Submenu 2',
-            icon: 'pi pi-fw pi-bookmark',
-            items: [
-              {
-                label: 'Submenu 2.1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                  { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                  { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                ]
-              },
-              {
-                label: 'Submenu 2.2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-              }
-            ]
-          }
-        ]
-      },
+      // {
+      //   label: 'Hierarchy',
+      //   items: [
+      //     {
+      //       label: 'Submenu 1',
+      //       icon: 'pi pi-fw pi-bookmark',
+      //       items: [
+      //         {
+      //           label: 'Submenu 1.1',
+      //           icon: 'pi pi-fw pi-bookmark',
+      //           items: [
+      //             { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+      //             { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+      //             { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+      //           ]
+      //         },
+      //         {
+      //           label: 'Submenu 1.2',
+      //           icon: 'pi pi-fw pi-bookmark',
+      //           items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       label: 'Submenu 2',
+      //       icon: 'pi pi-fw pi-bookmark',
+      //       items: [
+      //         {
+      //           label: 'Submenu 2.1',
+      //           icon: 'pi pi-fw pi-bookmark',
+      //           items: [
+      //             { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+      //             { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+      //           ]
+      //         },
+      //         {
+      //           label: 'Submenu 2.2',
+      //           icon: 'pi pi-fw pi-bookmark',
+      //           items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // },
       {
         label: 'Get Started',
         items: [
